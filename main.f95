9d6da74d764b66e99bd83f85a4383cb84b185f19
krilion08 <krilion583@gmail.com>
2021-01-26 01:00:13 +0300

    bug fixed

--- main.f95
+++ main.f95
@@ -1,30 +1,30 @@
 program bisection_solver
   implicit none
-  real(8), parameter :: eps=1.d-6
+  real(8), parameter :: eps=1.d-6     ! error of a solution
   real(8) :: a, b, f, c
   integer :: i
 
 
   c=1
-  read*, a, b
+  read*, a, b                         ! select range
   if (f(a)*f(b)>=0) then
     write (*,*) ("Wrong initial interval")
   error stop
   endif  
-  do while (abs(c)>eps)
+  do while (abs(f(c))>eps)
       c=(a+b)/2
       if (f(c)*f(b)<=0) then
         a=c
       else 
         b=c
       endif
-      print*, c
+      print*, "zero of the function is ",c
   enddo
   end program
 
 function f(x)
   real(8) :: x, f
-  f=25*x
+  f=-25*x+71.33*x**2-555
   return
   end function
 
